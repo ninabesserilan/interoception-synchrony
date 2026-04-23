8e32814f2f63179c663e83c48b809bfb9c4e8af7
Nina <nina.besser.ilan@gmail.com>
2026-04-23 17:26:55 +0200

    remove detren function from the rsa abbney pipeline - we will use a different approach in R

---
+++ json try/try.py
@@ -0,0 +1,7 @@
+import json
+
+with open('/Users/nina/Desktop/University of Vienna/PhD projects/Interoception and physiological synchrony/Empathy data/03_ibi_after_manual_coding_should_be_json/dashboardInputData_emp18 - jason/101_empathyhammer_ecg1_distress_ch1_ecg.json', "r") as f:
+    data = json.load(f)
+
+with open('/Users/nina/Desktop/University of Vienna/PhD projects/Interoception and physiological synchrony/Empathy data/03_ibi_after_manual_coding_should_be_json/dashboardInputData_emp18 - jason/101_empathyhammer_ecg2_distress_ch3_ecg.json', "r") as f:
+    data2 = json.load(f)
